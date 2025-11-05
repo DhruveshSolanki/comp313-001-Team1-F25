0d9065712836da47554852deddf26e2442add7cc
DhruveshSolanki <dvsolanki21@gmail.com>
2025-11-04 22:21:33 -0500

    DS: Initialize Backend

---
+++ api/feastflow-app/src/main/java/com/feastflow/feastflow_app/FeastflowAppApplication.java
@@ -0,0 +1,13 @@
+package com.feastflow.feastflow_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastflowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastflowAppApplication.class, args);
+	}
+
+}
---
+++ api/feastflow-app/src/test/java/com/feastflow/feastflow_app/FeastflowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feastflow.feastflow_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastflowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
