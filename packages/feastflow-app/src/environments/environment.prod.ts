4de9e8ad8835efeb9656199ca9235f492af91e5c
DhruveshSolanki <dvsolanki21@gmail.com>
2025-12-09 01:47:25 -0500

    DS:Fixed Host BE URL

--- packages/feastflow-app/src/environments/environment.prod.ts
+++ packages/feastflow-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://feastflow-app-567048823206.northamerica-northeast2.run.app/'
+  apiBaseUrl: 'https://feastflow-app-567048823206.northamerica-northeast2.run.app/api/v1'
 };
